ddbebd66cca2c42e126d1bd6edfb0a13921eafa7
yolan <yolan.pibrac@gmail.com>
2020-10-10 20:56:29 +0200

    test: delete test

--- desktop/desktopTest/urls.py
+++ desktop/desktopTest/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index1'),
+    path('', views.index, name='index'),
     path('<str:note_id>/detail/', views.detail, name='note_detail'),
     path('titles', views.getTitles, name='getTitles'),
 ]
