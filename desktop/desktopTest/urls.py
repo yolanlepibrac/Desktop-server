f82f04b11af93a77315bcfff1046e2f48b0feaa6
yolan <yolan.pibrac@gmail.com>
2020-10-10 20:39:29 +0200

    test:deploy for heroku

--- desktop/desktopTest/urls.py
+++ desktop/desktopTest/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index1'),
     path('<str:note_id>/detail/', views.detail, name='note_detail'),
     path('titles', views.getTitles, name='getTitles'),
 ]
