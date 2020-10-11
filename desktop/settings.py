e72a6b1aff4091cba07effce25adee075047acd7
yolan <yolan.pibrac@gmail.com>
2020-10-11 20:56:16 +0200

    feature(settings): enable cors for localhost

--- desktop/settings.py
+++ desktop/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'rest_framework',
+    'corsheaders',
     'desktopTest.apps.DesktopTestConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -39,6 +41,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000'
 ]
 
 ROOT_URLCONF = 'desktop.urls'
